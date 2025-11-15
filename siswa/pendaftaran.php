e23851d6d423423228711b00deec6b4a83fcbc71
Tulsoppp <alanjrx521@gmail.com>
2025-11-15 19:40:46 +0700

    Form Pendaftaran

--- siswa/pendaftaran.php
+++ siswa/pendaftaran.php
@@ -4,4 +4,170 @@ require_once "../cekLogin.inc";
 require_once "../includes/header.php";
 require_once "../includes/navbarSiswa.php";
  ?>
-<h2>SElamat mendaftar</h2>
+<div class="form_pendaftaran">
+    <h1>Form Pendaftaran Siswa Baru PPDB Sekolah Inklusi</h1>
+    <form action="proses_pendaftaran.php" method="POST" enctype="multipart/form-data" class="isi_pendaftaran">
+        
+        <h2>Data Pribadi Siswa</h2>
+        <hr>
+
+        <div class="form_isi">
+            <label for="nama_lengkap">Nama Lengkap :</label>
+            <input type="text" id="nama_lengkap" name="nama_siswa" required>
+        </div>
+
+        <div class="form_isi">
+            <label>Jenis Kelamin :</label>
+            <div class="radio-group-horizontal">
+                <input type="radio" id="laki-laki" name="jenis_kelamin" value="Laki-laki" required>
+                <label for="laki-laki">Laki-laki</label>
+                
+                <input type="radio" id="perempuan" name="jenis_kelamin" value="Perempuan">
+                <label for="perempuan">Perempuan</label>
+            </div>
+        </div>
+
+        <div class="form_isi">
+            <label>Agama :</label>
+            <input type="text" id="agama" name="agama_siswa" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="tempat_lahir">Tempat Lahir :</label>
+            <input type="text" id="tempat_lahir" name="tempat_lahir" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="tgl_lahir">Tanggal Lahir :</label>
+            <input type="date" id="tgl_lahir" name="tanggal_lahir" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="alamat_siswa">Alamat Siswa :</label>
+            <input type="text" id="alamat_siswa" name="alamat_siswa" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="hp_siswa">No HP Siswa :</label>
+            <input type="number" id="hp_siswa" name="no_hp_siswa" required>
+        </div>
+
+        <div class="form_isi">
+            <label for="jurusan">Pilihan Jurusan:</label>
+            <select id="jurusan" name="id_jurusan" required>
+                <option value="">-- Pilih Jurusan --</option>
+                <option value="1">IPA (Ilmu Pengetahuan Alam)</option>
+                <option value="2">IPS (Ilmu Pengetahuan Sosial)</option>
+            </select>
+        </div>
+        
+        <h2>Kebutuhan Khusus</h2>
+        <hr>
+        <div class="form_isi">
+            <label for="kebutuhan">Pilih jika siswa memiliki kebutuhan khusus/kekurangan:</label>
+            <select id="kebutuhan" name="id_kebutuhan" required>
+                <option value="">-- Pilih Kebutuhan --</option>
+                <option value="1">Normal</option>
+                <option value="2">Tunanetra</option>
+                <option value="3">Tunarungu</option>
+                <option value="4">Kesulitan Belajar</option>
+            </select>
+        </div>
+        <h2>Data Dokumen</h2>
+        <hr>
+        
+        <div class="form_isi">
+            <label for="kk">Nomor Kartu Keluarga :</label>
+            <input type="file" 
+                id="pas_foto" 
+                name="foto_siswa" 
+                accept=".jpg, .jpeg, .png" 
+                required>
+        </div>
+
+        <div class="form_isi">
+            <label for="akta">Nomor Akte Kelahiran :</label>
+            <input type="file" 
+                id="pas_foto" 
+                name="foto_siswa" 
+                accept=".jpg, .jpeg, .png" 
+                required>
+        </div>
+
+        <div class="form_isi">
+            <label for="ijazah">Nomor Ijazah :</label>
+            <input type="file" 
+                id="pas_foto" 
+                name="foto_siswa" 
+                accept=".jpg, .jpeg, .png" 
+                required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="pas_foto">Foto Pas Siswa (Upload) :</label>
+            <input type="file" 
+                id="pas_foto" 
+                name="foto_siswa" 
+                accept=".jpg, .jpeg, .png" 
+                required>
+        </div>
+
+        <h2>Data Wali</h2>
+        <hr>
+
+        <div class="form_isi">
+            <label for="nama_wali">Nama Wali :</label>
+            <input type="text" id="nama_wali" name="nama_wali" required>
+        </div>
+
+        <div class="form_isi">
+            <label>Status Wali (Orang Tua) :</label>
+            <div class="radio-group-horizontal">
+                <input type="radio" id="hidup" name="status_wali" value="Hidup" required>
+                <label for="hidup">Masih Hidup</label>
+                
+                <input type="radio" id="wafat" name="status_wali" value="Wafat">
+                <label for="wafat">Sudah Tidak Ada</label>
+            </div>
+        </div>
+
+        <div class="form_isi">
+            <label>Hubungan Wali :</label>
+            <div class="radio-group-horizontal">
+                <input type="radio" id="kandung" name="hubungan" value="Kandung" required>
+                <label for="kandung">Anak Kandung</label>
+                
+                <input type="radio" id="angkat" name="hubungan" value="Angkat">
+                <label for="angkat">Anak Angkat</label>
+                
+                <input type="radio" id="tiri" name="hubungan" value="Tiri">
+                <label for="tiri">Anak Tiri</label>
+            </div>
+        </div>
+        
+        <div class="form_isi">
+            <label for="pekerjaan_wali">Pekerjaan Wali :</label>
+            <input type="text" id="pekerjaan_wali" name="pekerjaan_wali" required>
+        </div>
+
+        <div class="form_isi">
+            <label for="alamat_wali">Alamat Wali :</label>
+            <input type="text" id="alamat_wali" name="alamat_wali" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="hp_wali">No HP Wali :</label>
+            <input type="number" id="hp_wali" name="no_hp_wali" required>
+        </div>
+        
+        <div class="form_isi">
+            <label for="gaji_wali">Gaji Wali :</label>
+            <input type="number" id="gaji_wali" name="slip_gaji" required>
+        </div>
+
+        <div class="form-actions">
+            <button type="submit">Kirim Pendaftaran</button>
+        </div>
+        
+    </form>
+</div>
