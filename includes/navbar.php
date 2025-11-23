e0e1ee695ebf394d528c04784ffa71a8052f2259
Achmad JIhad Fisabilillah <achmadjihadfisabilillah123@gmail.com>
2025-11-23 19:27:13 +0700

    dsds

--- includes/navbar.php
+++ includes/navbar.php
@@ -14,15 +14,6 @@
         </li>
     </ul>
 	<ul>
-		<li><a href="index.php">Beranda</a></li>
-		<li><a href="#isi">Profil Sekolah</a></li>
-		<li><a href="#vimisi">Visi & Misi</a></li>
-		<li><a href="#pendaftaran">Jadwal Pendaftaran Online
-		</a></li>
-		<li><a href="#alur">Alur Pndaftaran Online</a></li>
-		<li><a href="#detail">Detial Pendaftaran</a></li>
-	</ul>
-	<ul>
 		<li><a href="login.php"><button>Login</button></a></li>
 	</ul>
 </div>
